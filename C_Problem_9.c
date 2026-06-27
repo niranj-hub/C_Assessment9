c0886378d0c0ac89f9976014f1b07bd756ddf4f5
Niranjana <ponniranjana2006@gmail.com>
2026-06-27 21:07:03 +0530

    Create C_Problem_9.c

---
+++ C_Problem_9.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int num[5],i,j,newarray[5];
+    j=0;
+    for(i=0;i<5;i++)
+    {
+        printf("Enter a number:");
+        scanf("%d",&num[i]);
+    }
+    for(i=0;i<5;i++)
+    {
+       if(num[i]%2!=0)
+       {
+           newarray[j]=num[i];
+           j++;
+       }
+    }
+    for(i=0;i<j;i++)
+    {
+        printf("%d ",newarray[i]);
+    }
+}
